dc850fb1f98fa88c592a62c5e7c9e893de62a713
standwall5 <johnpatricksalen890@gmail.com>
2024-10-30 12:55:23 +0800

    fix: added languages and footer lightmode

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ function myFunction() {
     var element = document.body;
     var boxes = document.getElementsByClassName("box"); // HTMLCollection
     var lines = document.getElementsByClassName("line");
+    var footer = document.getElementsByClassName("footer"); // This is an array
     element.classList.toggle("light-mode");
 
-    // Loop through all elements with the class "box"
+    // Loop through all elements with the class "box, line and"
     for (var i = 0; i < boxes.length; i++) {
         boxes[i].classList.toggle("light-mode");
     }
@@ -12,4 +13,5 @@ function myFunction() {
     for (var i = 0; i < lines.length; i++) {
         lines[i].classList.toggle("light-mode");
     }
+        footer[0].classList.toggle("light-mode");
   }
